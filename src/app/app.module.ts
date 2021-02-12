5bbe469534640037919084e0420bf9798c7c36b8
Mahmoud Zakaria <zakaria.forwork@gmail.com>
2021-02-12 08:19:31 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { QuizComponent } from './components/quiz/quiz/quiz.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
